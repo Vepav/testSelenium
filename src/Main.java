df2ebd9f9f602a11faa920bdc33e3b5ba61d3194
Dragan <qa-prepodne@gmail.com>
2018-06-13 12:13:42 +0200

    add classes framework and new command

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,79 @@
 
+import framework.Helper;
+import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Main {
 
- 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        
+
         driver.get("http://bvtest.school.cubes.rs/login");
-        
+
         WebElement emailField = driver.findElement(By.name("email"));
         emailField.sendKeys("qa@cubes.rs");
-        
+
         WebElement passwordField = driver.findElement(By.name("password"));
         passwordField.sendKeys("cubesqa");
-        
+
         WebElement loginButton = driver.findElement(By.className("btn-primary"));
         loginButton.click();
-        
+
+        System.out.println("Page title is: " + driver.getTitle() + "!");
+
         WebElement signaturesLink = driver.findElement(By.linkText("Signatures"));
         signaturesLink.click();
+
+        System.out.println("Page title is: " + driver.getTitle() + "!");
+
+        //Categories
+        // go to Categories page
+        
+        WebElement categoriesLink = driver.findElement(By.partialLinkText("Cate"));
+        categoriesLink.click();
+        
+        System.out.println("Page title is: " + driver.getTitle());
+
+                
+        // Click on 'Add category' button
+        WebElement addCategoryButton = driver.findElement(By.className("pull-right"));
+        addCategoryButton.click();        
+        
+                
+        //Fill Title field
+        WebElement titleField = driver.findElement(By.id("title"));
+        titleField.sendKeys(Helper.getRandomText());
+        
+         
+        // Click on 'Save' button
+        WebElement saveButton = driver.findElement(By.id("save-category-button"));
+        saveButton.click();
+        
+        //alternativni nacin
+        //driver.findElement(By.id("save-category-button")).click();
+        
+        
+        //REGIONS
+        
+        WebElement navBar = driver.findElement(By.className("navbar-nav"));
+        List<WebElement> liTags = navBar.findElements(By.tagName("li"));
+        liTags.get(4).click();
+        
+        
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement addPortalButton = wait.until(ExpectedConditions.elementToBeClickable(By.className("pull-right")));
+        addPortalButton.click();
         
         
         
-       
-        //driver.quit();
-        
+        Thread.sleep(7000);        
+        driver.quit();
     }
-    
+
 }
---
+++ src/framework/Helper.java
@@ -0,0 +1,19 @@
+
+package framework;
+
+public class Helper {
+    
+    public static String getRandomText() {
+     
+//    int random = (int)(Math.random() * 250 + 1);
+//    String randomText = "DK" + random;
+//    return randomText;
+//    
+    return "DK" + (int) (Math.random() * 500 + 1);
+    
+}
+   
+    
+   
+    
+}
